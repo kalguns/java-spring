e9ce89b367918e3d5cb2756a9a97f78213093f7f
kalguns <kalguns@gmail.com>
2020-10-21 22:30:53 +0100

    DI-Constructor Injection

---
+++ Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,20 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java implements Course {
+
+    int x;
+
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: " + (20 + extraSessions.getHours()));
+    }
+}
