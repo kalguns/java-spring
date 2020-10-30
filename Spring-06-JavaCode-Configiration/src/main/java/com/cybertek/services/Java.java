1dd4b30c0653a2f00953be42b5493d2bfdd7b59d
kalguns <kalguns@gmail.com>
2020-10-30 21:34:41 +0100

    Java-Code Configuration

--- Spring-06-JavaCode-Configiration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configiration/src/main/java/com/cybertek/services/Java.java
@@ -1,11 +1,37 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
-import org.springframework.stereotype.Component;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Value;
 
-@Component
+import java.util.Arrays;
+
 public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: 30");
+        System.out.println("Weekly teaching hours: " + (30 + extraSessions.getHours()));
+    }
+
+    @Value("JD1")
+    private String batch;
+    @Value("${instructor}")
+    private String instructor;
+    @Value("${days}")
+    private String[] days;
+
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
     }
 }
