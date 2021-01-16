9c302d728367dd46777626c8274ddff5eb3f46ff
kalguns <kalguns@gmail.com>
2021-01-16 23:06:10 +0100

    ORM-Lab-CinemaApp

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -3,6 +3,8 @@ package com.cybertek.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
@@ -16,7 +18,7 @@ public class Cinema extends BaseEntity {
     private String name;
     private String sponsoredName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
     private Location location;
 
     public Cinema(String name, String sponsoredName) {
