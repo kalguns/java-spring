efb89fa664054cfaf2ad779de24d4754daee6b6c
kalguns <kalguns@gmail.com>
2021-01-18 18:21:31 +0100

    ORM-JPQL-Named Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -68,4 +68,12 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
     List<Employee> getEmployeeByEmailIsNotNull();
 
+    //Sort Salary in ascending order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> getEmployeeBySalaryOrderByAsc();
+
+    //Sort Salary in descending order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary DESC")
+    List<Employee> getEmployeeBySalaryOrderByDesc();
+
 }
