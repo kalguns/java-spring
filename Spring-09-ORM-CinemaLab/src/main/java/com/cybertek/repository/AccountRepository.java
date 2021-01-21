82a8a32e075ef1ce149f13566734f243c914e06f
Mustafa Aksu <63937467+kalguns@users.noreply.github.com>
2021-01-21 12:23:16 +0100

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/AccountRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/AccountRepository.java
@@ -34,7 +34,7 @@ public interface AccountRepository extends JpaRepository<Account,Long> {
    List<Account> fetchAllJPQL();
 
     //Write a JPQL query to list all admin accounts
-    @Query("SELECT a FROM Account a WHERE a.role='Admin'")
+    @Query("SELECT a FROM Account a WHERE a.role='USER'")
     List<Account> fetchAdminUsers();
 
     //Write a JPQL query to sort all accounts with age
