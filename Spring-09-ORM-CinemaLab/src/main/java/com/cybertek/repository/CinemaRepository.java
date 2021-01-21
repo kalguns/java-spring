985f9eb7132a193106a8d96fedf242c35e027f12
Mustafa Aksu <63937467+kalguns@users.noreply.github.com>
2021-01-21 12:46:02 +0100

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -2,6 +2,7 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,9 +24,18 @@ public interface CinemaRepository extends JpaRepository<Cinema,Long> {
     List<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredname);
     // ------------------- JPQL QUERIES ------------------- //
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id=?1")
+    String fetchByIdJPQL(Integer id);
+
     // ------------------- Native QUERIES ------------------- //
     //Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c JOIN location l on l.id=c.location_id WHERE l.country=?1",nativeQuery = true)
+    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
+
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinama WHERE name ILIKE concat('%',?1,'%') OR sponsored_name ILIKE concat('%',?1,'%')",nativeQuery = true)
+    List<Cinema> retrieveAllByNameOrSponsoredName(String pattern);
+
     //Write a native query to sort all cinemas by name
     //Write a native query to distinct all cinemas by sponsored name
 }
